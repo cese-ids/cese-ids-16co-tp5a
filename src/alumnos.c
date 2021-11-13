2398847fe033910b0a6343741e052fd6e164c1fe
AndreGarciaC <andregarciace@gmail.com>
2021-11-13 10:24:05 -0500

    Se agregan los datos de la alumna García, Andrea

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,13 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+bool AndreaGarcia(char * cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "GARCÍA",
+        .nombres = "Andrea Estefanía",
+        .documento = "0103996112",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
 /*=====[Implementations of private functions]================================*/
